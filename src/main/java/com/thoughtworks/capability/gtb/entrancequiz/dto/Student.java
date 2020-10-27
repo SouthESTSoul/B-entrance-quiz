b86ccba9c87fe26c6def46eefc077b40ea28ec2d
g992987642 <992987642@qq.com>
2020-10-27 18:46:26 +0800

    [guhao]:add lombok dependencies

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/dto/Student.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/dto/Student.java
@@ -1,5 +1,15 @@
 package com.thoughtworks.capability.gtb.entrancequiz.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Student {
+    int id;
+
 }
